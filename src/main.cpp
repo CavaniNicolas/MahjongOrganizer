962b09c2cb29bf886a3718a5febb047620a0eebf
CavaniNicolas <cavani.nicolas6@gmail.com>
2022-06-07 15:41:38 -0400

    [dev] save old code in order to work on algo only

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-
 #include <iostream>
 #include <string>
 
 #include <mahjong/json.hpp>
 
 #include "mahjong/Player.hpp"
-#include "mahjong/PlayerModel.hpp"
 #include "mahjong/PlayerParser.hpp"
 #include "mahjong/Room.hpp"
 
@@ -17,43 +12,27 @@ int main(int argc, char* argv[])
 {
     using json = nlohmann::json;
 
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/components/main.qml")));
-    if(engine.rootObjects().isEmpty())
-    {
-        return -1;
-    }
-
     PlayerParser pp(std::string(RESOURCE_PATH) + "mahjong.json");
 
     Room room(pp.readPlayersFromFile());
-    //    room.displayAllPlayers();
+    // room.displayAllPlayers();
 
-    PlayerModel playerModel;
-    playerModel.addPlayer(QPlayer("jean", "dupont", QString::fromStdString(getStringFromLevel(Level::Beginner))));
-    playerModel.addPlayer(QPlayer("will", "smith", QString::fromStdString(getStringFromLevel(Level::Leisure))));
-    playerModel.addPlayer(QPlayer("el boban", "novic", QString::fromStdString(getStringFromLevel(Level::Competitive))));
-    playerModel.addPlayer(
-      QPlayer("dernier", "dernier", QString::fromStdString(getStringFromLevel(Level::Competitive))));
+    //    room.addNewPlayer(Player("un", "un", Level::Beginner));
+    //    room.addNewPlayer(Player("deux", "deux", Level::Leisure));
+    //    room.addNewPlayer(Player("trois", "trois", Level::Competitive));
+    //    room.addNewPlayer(Player("quatre", "quatre", Level::Beginner));
+    //    room.addNewPlayer(Player("cinq", "cinq", Level::Leisure));
+    //    room.addNewPlayer(Player("six", "six", Level::Competitive));
+    //    room.addNewPlayer(Player("sept", "sept", Level::Beginner));
+    //    room.addNewPlayer(Player("huit", "huit", Level::Leisure));
+    //    room.addNewPlayer(Player("neuf", "neuf", Level::Competitive));
 
-    //      on expose l'object playerModel au qml, pour pouvoir connect le signal qml au
-    //      slot cpp dans le qml
-    //      (onNewPlayerFormSaved.connect(playerModel.newPlayerFormSaved); dans
-    //      NewPlayerForm.qml)
-    engine.rootContext()->setContextProperty("playerModel", &playerModel);
+    // room.removePlayerFromIndex(room.getNbOfPlayers() - 1);
 
-    room.addNewPlayer(Player("neuf", "neuf", Level::Competitive));
-
-    room.removePlayerFromIndex(room.getNbOfPlayers() - 1);
-
-    //    room.displayAllPlayers();
     std::cout << room.getNbOfPlayers() << std::endl;
 
     pp.writePlayersInFile(room.getPlayersJson());
+    // room.removePlayerFromId(32);
 
-    room.removePlayerFromId(32);
-
-    return app.exec();
+    return 0;
 }
